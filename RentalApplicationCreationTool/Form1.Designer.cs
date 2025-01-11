4489e33cf7be9222facfe58dfe773e70d1578a58
daichi <kabata0210@icloud.com>
2025-01-11 22:39:55 +0900

    画面設計

--- RentalApplicationCreationTool/Form1.Designer.cs
+++ RentalApplicationCreationTool/Form1.Designer.cs
@@ -28,12 +28,507 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            dateTimePicker2 = new DateTimePicker();
+            dateTimePicker3 = new DateTimePicker();
+            dateTimePicker4 = new DateTimePicker();
+            groupBox1 = new GroupBox();
+            textBox8 = new TextBox();
+            checkBox10 = new CheckBox();
+            checkBox9 = new CheckBox();
+            checkBox8 = new CheckBox();
+            checkBox7 = new CheckBox();
+            checkBox6 = new CheckBox();
+            checkBox5 = new CheckBox();
+            checkBox4 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            textBox7 = new TextBox();
+            groupBox2 = new GroupBox();
+            checkBox11 = new CheckBox();
+            checkBox12 = new CheckBox();
+            checkBox13 = new CheckBox();
+            comboBox1 = new ComboBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 0;
+            label1.Text = "申請日";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(31, 15);
+            label2.TabIndex = 1;
+            label2.Text = "氏名";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(55, 15);
+            label3.TabIndex = 2;
+            label3.Text = "電話番号";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 102);
+            label4.Name = "label4";
+            label4.Size = new Size(31, 15);
+            label4.TabIndex = 3;
+            label4.Text = "住所";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(73, 12);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 4;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 131);
+            label5.Name = "label5";
+            label5.Size = new Size(43, 15);
+            label5.TabIndex = 5;
+            label5.Text = "団体名";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 157);
+            label6.Name = "label6";
+            label6.Size = new Size(55, 15);
+            label6.TabIndex = 6;
+            label6.Text = "使用目的";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(12, 192);
+            label7.Name = "label7";
+            label7.Size = new Size(55, 15);
+            label7.TabIndex = 7;
+            label7.Text = "使用日時";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(322, 44);
+            label8.Name = "label8";
+            label8.Size = new Size(55, 15);
+            label8.TabIndex = 8;
+            label8.Text = "使用室名";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(322, 218);
+            label9.Name = "label9";
+            label9.Size = new Size(55, 15);
+            label9.TabIndex = 9;
+            label9.Text = "使用人数";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(322, 244);
+            label10.Name = "label10";
+            label10.Size = new Size(79, 15);
+            label10.TabIndex = 10;
+            label10.Text = "使用附属設備";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(310, 361);
+            label11.Name = "label11";
+            label11.Size = new Size(55, 15);
+            label11.TabIndex = 11;
+            label11.Text = "免除申請";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(73, 41);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(73, 70);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(73, 99);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(179, 99);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(73, 128);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 16;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(73, 157);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 17;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(73, 186);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(200, 23);
+            dateTimePicker2.TabIndex = 18;
+            // 
+            // dateTimePicker3
+            // 
+            dateTimePicker3.Format = DateTimePickerFormat.Time;
+            dateTimePicker3.Location = new Point(73, 215);
+            dateTimePicker3.Name = "dateTimePicker3";
+            dateTimePicker3.ShowUpDown = true;
+            dateTimePicker3.Size = new Size(200, 23);
+            dateTimePicker3.TabIndex = 19;
+            // 
+            // dateTimePicker4
+            // 
+            dateTimePicker4.Format = DateTimePickerFormat.Time;
+            dateTimePicker4.Location = new Point(73, 244);
+            dateTimePicker4.Name = "dateTimePicker4";
+            dateTimePicker4.ShowUpDown = true;
+            dateTimePicker4.Size = new Size(200, 23);
+            dateTimePicker4.TabIndex = 20;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox8);
+            groupBox1.Controls.Add(checkBox10);
+            groupBox1.Controls.Add(checkBox9);
+            groupBox1.Controls.Add(checkBox8);
+            groupBox1.Controls.Add(checkBox7);
+            groupBox1.Controls.Add(checkBox6);
+            groupBox1.Controls.Add(checkBox5);
+            groupBox1.Controls.Add(checkBox4);
+            groupBox1.Controls.Add(checkBox3);
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Location = new Point(409, 44);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(309, 163);
+            groupBox1.TabIndex = 21;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(191, 120);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 11;
+            // 
+            // checkBox10
+            // 
+            checkBox10.AutoSize = true;
+            checkBox10.Location = new Point(128, 122);
+            checkBox10.Name = "checkBox10";
+            checkBox10.Size = new Size(57, 19);
+            checkBox10.TabIndex = 9;
+            checkBox10.Text = "その他";
+            checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            checkBox9.AutoSize = true;
+            checkBox9.Location = new Point(128, 97);
+            checkBox9.Name = "checkBox9";
+            checkBox9.Size = new Size(62, 19);
+            checkBox9.TabIndex = 8;
+            checkBox9.Text = "資料室";
+            checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            checkBox8.AutoSize = true;
+            checkBox8.Location = new Point(6, 122);
+            checkBox8.Name = "checkBox8";
+            checkBox8.Size = new Size(62, 19);
+            checkBox8.TabIndex = 7;
+            checkBox8.Text = "調理室";
+            checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            checkBox7.AutoSize = true;
+            checkBox7.Location = new Point(128, 72);
+            checkBox7.Name = "checkBox7";
+            checkBox7.Size = new Size(74, 19);
+            checkBox7.TabIndex = 6;
+            checkBox7.Text = "和室①②";
+            checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Location = new Point(128, 47);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new Size(62, 19);
+            checkBox6.TabIndex = 5;
+            checkBox6.Text = "和室②";
+            checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new Point(128, 22);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(62, 19);
+            checkBox5.TabIndex = 4;
+            checkBox5.Text = "和室①";
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(6, 97);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(62, 19);
+            checkBox4.TabIndex = 3;
+            checkBox4.Text = "図書室";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(6, 72);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(86, 19);
+            checkBox3.TabIndex = 2;
+            checkBox3.Text = "会議室①②";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(6, 47);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(74, 19);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "会議室②";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(6, 22);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(74, 19);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "会議室①";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(409, 215);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 22;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox9);
+            groupBox2.Controls.Add(checkBox11);
+            groupBox2.Controls.Add(checkBox12);
+            groupBox2.Location = new Point(409, 244);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(309, 82);
+            groupBox2.TabIndex = 23;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // checkBox11
+            // 
+            checkBox11.AutoSize = true;
+            checkBox11.Location = new Point(6, 22);
+            checkBox11.Name = "checkBox11";
+            checkBox11.Size = new Size(62, 19);
+            checkBox11.TabIndex = 10;
+            checkBox11.Text = "冷暖房";
+            checkBox11.UseVisualStyleBackColor = true;
+            // 
+            // checkBox12
+            // 
+            checkBox12.AutoSize = true;
+            checkBox12.Location = new Point(6, 47);
+            checkBox12.Name = "checkBox12";
+            checkBox12.Size = new Size(57, 19);
+            checkBox12.TabIndex = 0;
+            checkBox12.Text = "その他";
+            checkBox12.UseVisualStyleBackColor = true;
+            // 
+            // checkBox13
+            // 
+            checkBox13.AutoSize = true;
+            checkBox13.Location = new Point(409, 360);
+            checkBox13.Name = "checkBox13";
+            checkBox13.Size = new Size(120, 19);
+            checkBox13.TabIndex = 1;
+            checkBox13.Text = "使用料の免除申請";
+            checkBox13.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "1", "2", "3" });
+            comboBox1.Location = new Point(579, 358);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 24;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(69, 45);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(100, 23);
+            textBox9.TabIndex = 11;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(179, 41);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(100, 23);
+            textBox10.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox10);
+            Controls.Add(comboBox1);
+            Controls.Add(checkBox13);
+            Controls.Add(groupBox2);
+            Controls.Add(textBox7);
+            Controls.Add(groupBox1);
+            Controls.Add(dateTimePicker4);
+            Controls.Add(dateTimePicker3);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private DateTimePicker dateTimePicker1;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private DateTimePicker dateTimePicker2;
+        private DateTimePicker dateTimePicker3;
+        private DateTimePicker dateTimePicker4;
+        private GroupBox groupBox1;
+        private CheckBox checkBox10;
+        private CheckBox checkBox9;
+        private CheckBox checkBox8;
+        private CheckBox checkBox7;
+        private CheckBox checkBox6;
+        private CheckBox checkBox5;
+        private CheckBox checkBox4;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private GroupBox groupBox2;
+        private CheckBox checkBox11;
+        private CheckBox checkBox12;
+        private CheckBox checkBox13;
+        private ComboBox comboBox1;
+        private TextBox textBox9;
+        private TextBox textBox10;
     }
 }
--- RentalApplicationCreationTool/obj/Debug/net8.0-windows/RentalApplicationCreationTool.AssemblyInfo.cs
+++ RentalApplicationCreationTool/obj/Debug/net8.0-windows/RentalApplicationCreationTool.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentalApplicationCreationTool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62479a620754ac3d355c0922f9796cf406159fd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentalApplicationCreationTool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentalApplicationCreationTool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
